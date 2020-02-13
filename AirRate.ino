b1b9919635ca2cd9f83563e39ba7f6ea3701af0f
Alexa Ognjanovic <alexa.ognjanovic@gmail.com>
2020-02-13 17:09:28 +0100

    PMS7003 done, NTPClient Done, WebServer and EEPROM SSID PASSWORD next

--- AirRate.ino
+++ AirRate.ino
@@ -1,5 +1,8 @@
 #include <Adafruit_BME280.h>
 #include <Adafruit_Sensor.h>
+#include <NTPClient.h>
+#include <PMS.h>
+#include <SoftwareSerial.h>
 #include <WiFi.h>
 #include <Wire.h>
 
@@ -7,22 +10,30 @@
 
 #define BAR_HEIGHT 12
 #define SEALEVELPRESSURE_HPA (1013.25)
-#define BAUD_RATE 115200
+#define BAUD_RATE 9600
 #define CONTRAST 255
 #define BME_LEN 4
 #define AIR_LEN 4
 #define SPLITTER_WIDTH 3
+#define TX 16
+#define RX 0
 
 const char* ssid = "";      // WiFi SSID here
 const char* password = "";  // Password here
 
 // WiFiServer server(80);  // Server on port 80
+WiFiUDP udp;
+NTPClient time_client(udp);
 
 SSD1306Wire display(0x3c, 5, 4);  // Display on pin 5 and 4
 Adafruit_BME280 bme;
 
+SoftwareSerial pms_serial(TX, RX);
+PMS pms(pms_serial);
+PMS::DATA pms_data;
+
 String air_labels[AIR_LEN] = {"1:", "2.5:", "10:"};
-int air_values[AIR_LEN] = {0, 0, 0};
+int air_values[AIR_LEN] = {0, 0, 10};
 
 String bme_labels[BME_LEN] = {"Tempera.:", "Pritisak:", "Vlaznost:", "Visina:"};
 String bme_unit[BME_LEN] = {"C", "hPa", "%", "m"};
@@ -30,7 +41,8 @@ int bme_values[BME_LEN] = {0, 0, 0, 0};
 
 void setup() {
     // Setting up serial at BAUD_RATE
-    Serial.begin(BAUD_RATE);
+    pms_serial.begin(BAUD_RATE);
+    Serial.begin(115200);
 
     // Setting up display
     display.init();
@@ -43,8 +55,9 @@ void setup() {
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
-        Serial.print(".");
+        Serial.println(".");
     }
+    time_client.begin();
 
     setup_draw();
 
@@ -80,13 +93,26 @@ void fetch_sensors() {
     bme_values[0] = bme.readTemperature();
     bme_values[3] = bme.readAltitude(SEALEVELPRESSURE_HPA);
     bme_values[2] = bme.readHumidity();
+
+    if (pms.read(pms_data)) {
+        air_values[0] = pms_data.PM_AE_UG_1_0;
+        air_values[1] = pms_data.PM_AE_UG_2_5;
+        air_values[2] = pms_data.PM_AE_UG_10_0;
+    }
+}
+
+void draw_time() {
+    time_client.update();
+    display.drawString(display.getWidth() / 4 * 3, 0, String(time_client.getHours()) + ":" + String(time_client.getMinutes()));
 }
 
 void loop() {
     display.clear();
     fetch_sensors();
     setup_draw();
+    draw_time();
     draw_bme();
     draw_air();
     display.display();
+    delay(500);
 }
